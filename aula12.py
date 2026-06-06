b896b3f384ec1b4d6859ce74dd88fea8e5240d0c
Davi Ramalho <davicel2015@gmail.com>
2026-06-06 12:10:36 -0300

    ex036
    Resolvendo o ex036 do curso mundo 2

---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = str(input("Digite seu nome: "))
+
+if nome == "Davi":
+    print(f"Que nome bonito {nome}!")
+elif nome == "Ana" or nome == "Pedro":
+    print(f"Que nome legal {nome}!")
+elif nome == "Lula" or nome == "Alexandre":
+    print(f"Seu nome é horrivel {nome}!")
+else:
+    print(f"Olá tubo bem {nome}!")
+print("Como posso de ajudar hoje?")
--- ex036.py
+++ ex036.py
@@ -1,3 +1,29 @@
-print ("testando Github")
-print("Segundo teste de arquivo editado")
-print("ultimo")
+valor_casa = float(input("Digite o valor da casa em R$: "))
+salario = float(input("Digite o seu salario em R$: "))
+parcela = int(input("Digite em quantas vezes você quer pagar: "))
+
+analise = salario * 0.30
+valor_parcela = valor_casa / parcela
+print(f"30% = {analise}")
+print(valor_parcela)
+
+if valor_parcela <= analise:
+    print(f"""
+    =========  FINANCEIRO  =========
+    APROVADO
+    
+    VALOR DA CASA:     R$ {valor_casa:.2f}
+    FINANCIAMENTO EM:     {parcela}x
+    VALOR DA PARCELA:  R$ {valor_parcela:.2f}
+    ================================
+    """)
+elif valor_parcela > analise:
+    print(f"""
+        =========  FINANCEIRO  =========
+        REPROVADO
+
+        VALOR DA CASA:     R$ {valor_casa:.2f}
+        FINANCIAMENTO EM:     {parcela}x
+        VALOR DA PARCELA:  R$ {valor_parcela:.2f}
+        ================================
+        """)
