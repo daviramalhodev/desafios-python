32e646e3f47ed653e1c64aa317b42cce4363b737
Davi Ramalho <davicel2015@gmail.com>
2026-07-23 17:11:47 -0300

    ex40

---
+++ ex040.py
@@ -0,0 +1,13 @@
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+if media < 5:
+    print(f"Sua média foi {media}, sendo assim está reprovado! ")
+elif media >= 5 and media <= 6.9:
+    print(f"Sua media foi {media} nesse caso vai precisar realizar a recuperação! ")
+else:
+    print(f"Parabéns sua media {media} então voce foi aprovado! ")
